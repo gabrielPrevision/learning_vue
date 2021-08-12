e54592174d0dbbff27e1e528004ba54a75201efb
Gabriel <gabriel.morais@prevision.com.br>
2021-08-12 11:32:27 -0300

    feat: vue-router implemented

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,22 @@
 <template lang="pug">
-  div#app
-    Header
-    wrapperRows
-  
+  v-app
+    v-content
+      router-view
 </template>
 
 <script>
-import WrapperRows from './components/wrapperRows'
-import Header from './components/header'
-
 export default {
   name: 'App',
-  components: {
-    Header,
-    WrapperRows
-  }
-}
+};
 </script>
 
 <style>
 *{
   box-sizing: border-box;
-}
-#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  max-width: 500px;
-  padding: 10px;
-  background: rgb(117, 158, 158);
-  align-self: center;
 }
 </style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  div#game
+    Header
+    wrapperRows
+  
+</template>
+
+<script>
+import WrapperRows from '../components/wrapperRows'
+import Header from '../components/header'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    WrapperRows
+  }
+}
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+}
+
+#game {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 500px;
+  padding: 10px;
+  background: rgb(117, 158, 158);
+  align-self: center;
+}
+</style>
